ace0eb438bcc91449311e3324f157df2d5963b55
Fan <243832027@qq.com>
2019-06-22 08:15:17 +0800

    蓝图完成

--- Manager.py
+++ Manager.py
@@ -2,19 +2,18 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from info import create_app, db
 
+# manage.py是程序启动的入口，只关心启动的相关参数以及内容，不关心具体该
+# 如果创建app或者相关业务逻辑
 
-app=create_app("development")
-# Flask-script
+# 通过指定的配置名字创建对应配置的app
+# create_app 就类似于工厂方法
+app = create_app('development')
+
 manager = Manager(app)
-# 数据库迁移
+# 将 app 与 db 关联
 Migrate(app, db)
+# 将迁移命令添加到manager中
 manager.add_command('db', MigrateCommand)
-
-
-@app.route('/')
-def index():
-    return 'index'
-
 
 if __name__ == '__main__':
     manager.run()
--- config.py
+++ config.py
@@ -1,30 +1,44 @@
-import redis
+import logging
+from redis import StrictRedis
+
 
 class Config(object):
-    """工程配置信息"""
+    """项目的配置"""
 
-    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
+    SECRET_KEY = "iECgbYWReMNxkRprrzMo5KAQYnb2UeZ3bwvReTSt+VSESW0OB8zbglT+6rEcDW9X"
 
-    # 数据库的配置信息
+    # 为数据库添加配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/flask"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # redis配置
+    # Redis的配置
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
-    # flask_session的配置信息
-    SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
-    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
-    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
+    # Session保存配置
+    SESSION_TYPE = "redis"
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 指定 Session 保存的 redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+
+    # 设置日志等级
+    LOG_LEVEL = logging.DEBUG
+
 
 class DevelopmentConfig(Config):
+    """开发环境下的配置"""
     DEBUG = True
 
 
 class ProductionConfig(Config):
+    """生产环境下的配置"""
     DEBUG = False
+    LOG_LEVEL = logging.WARNING
 
 
 class TestingConfig(Config):
